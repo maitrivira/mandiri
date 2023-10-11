7edff3123184d8232d33fdf4339e0bd93d34112d
Maitri Vira <maitrivira2@gmail.com>
2023-10-11 15:10:52 +0700

    Initial Commit

---
+++ BankMandiri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankMandiri
+//
+//  Created by Maitri Vira on 11/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
